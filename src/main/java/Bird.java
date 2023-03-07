2427cdcde0d95531d12cd78526da5bfb84aeac9c
Eddie Padilla <dediosprogramming@gmail.com>
2023-03-07 23:07:58 +0000

    ternary operator lab complete

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -27,25 +27,13 @@
  * 
  * 
  */
-public class Bird {
-    /**
-     * A variable (in this context, often referred to as a field), which represents the age of the bird.
-     */
+
+ public class Bird {
     public int ageMonths;
 
-    /**
-     * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
-     * No need to change anything here, for now.
-     */
-    public Bird(){
-
-    }
-    /**
-     * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
-     *         greater than or equal to 3.
-     */
-    public String sound(){
-        return "";
+    public String sound() {
+        String birdSound = (ageMonths < 3) ? "Tweet" : "Chirp";
+        return birdSound;
     }
 
 }
